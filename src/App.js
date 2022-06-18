075c0b8532bfb112f3976e8aedebe0bb785b18f5
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-06-18 07:39:48 -0400

    new project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { AppContainer } from "./components/Container.style";
-import { StyledButton } from "./components/Button.style";
-import { GlobalStyles } from "./components/Global.style";
 
 function App() {
-  return (
-    <AppContainer>
-      <GlobalStyles />
-      <StyledButton buttonLabel="Click Here" backgroundColor="violet"></StyledButton>
-    </AppContainer>
-  );
+  return <h1>Hello world</h1>;
 }
 
 export default App;
--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({ className, buttonLabel }) => {
-  return <button className={className}>{buttonLabel}</button>;
-};
-
-export default Button;
