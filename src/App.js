fc9858cb9177b42e39f3c40fd660594d8a1361cf
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-06-22 07:25:29 -0400

    finished styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
